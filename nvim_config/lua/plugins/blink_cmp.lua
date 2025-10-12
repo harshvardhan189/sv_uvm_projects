47b969ddf41d839c4cc6a6984f22a764cee4439c
harshvardhan189 <harshvardhan189@gmail.com>
2025-10-12 15:14:33 +0530

    remove nvim config

--- nvim_config/lua/plugins/blink_cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "saghen/blink.cmp",
-  version = "v0.*",
-  dependencies = {
-    { "saghen/blink.compat", optional = true, version = "*" },
-    "rafamadriz/friendly-snippets",
-  },
-  opts = {}
-}
--- nvim_config/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "lexghergh/nvim-tmux-navigation" },
-  config = function()
-    require("lualine").setup({
-      options = { theme = "dracula" },
-    })
-  end,
-}
