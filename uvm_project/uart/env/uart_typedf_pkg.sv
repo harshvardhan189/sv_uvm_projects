f5d82eae262bb537d9dae1066eb6509e9e4ff616
harshvardhan189 <harshvardhan189@gmail.com>
2026-02-17 23:05:44 +0530

    add required uart files

---
+++ uvm_project/uart/env/uart_typedf_pkg.sv
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------
+// Project : UART
+// class   : uart_typedef_pkg
+//-----------------------------------------------------------------
+
+package uart_typedef_pkg;
+  typedef enum (IDLE,START_BIT,DATA,PARITY,STOP_BIT) state_e;
+
+endpackage
--- uvm_project/uart/uart_agent_top/uart_agent_top.sv
+++ uvm_project/uart/uart_agent_top/uart_agent_top.sv
@@ -6,19 +6,14 @@ class uart_agent_top extends uvm_env;
 
   `uvm_component_utils(uart_agent_top)
 
-  uart_agent agnth;
+  uart_agent  agnth;
   uart_config Hcfg;
-  bridge_config cfg;
 
   function new(string name = "uart_agent_top", uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    if (!uvm_config_db#(bridge_config)::get(this, "", "bridge_config", cfg))
-      `uvm_fatal("CONFIG", "can't get config form uart agent top")
-
-
     super.build_phase(phase);
     agnth = uart_agent::type_id::create("agnth", this);
     uvm_config_db#(uart_config)::set(this, "*", "uart_config", cfg.Hcfg);
